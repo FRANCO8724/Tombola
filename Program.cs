20da5a0648b180422d016f11ef1b3f0f9e1fd14b
Riccardo Arrigoni <riccardo.arrigoni19@gmail.com>
2023-01-10 19:04:48 +0100

    Program.cs

--- Program.cs
+++ Program.cs
@@ -12,50 +12,131 @@ class program
     public static void Main(string[] args)
     {
         // Variabili
-        int giocatore1, giocatore2;
         Random random = new Random();
+        List<int> num = Enumerable.Range(1, 90).ToList();
 
         //giocatore1
-
-            Console.SetCursorPosition(25, 0);
+        for(int i = 0; i < 20; i++)
+        {
+            Console.SetCursorPosition(30, 0);
             Console.WriteLine("Giocatore 1");
 
-        //fila 1
-        for (int  j= 0; j < 5; j++)
+            for (int j = 28; j < 48 ; j++)
+            {
+                Console.WriteLine("-");
+                Console.SetCursorPosition(j, 4);
+            }
+            for (int j = 28; j < 50; j++)
+            {
+                Console.WriteLine("-");
+                Console.SetCursorPosition(j, 6);
+            }
+        }
+        //Cartella 1
+        for (int j = 0; j < 16; j++)
         {
-
-                Thread.Sleep(j);
+            Thread.Sleep(j);
             int b = random.Next(1, 90);
+
+            if (j < 6)
+            {
+
+
 
                 if (b == 10 || b == 20 || b == 30 || b == 40 || b == 50 || b == 60 || b == 70 || b == 80 || b == 90)
                 {
-                    Console.SetCursorPosition(43, 3);
-                    Console.WriteLine(b);
+
+                    Console.SetCursorPosition(48, 3);
+                    Console.WriteLine(" " + b);
                 }
                 else
                 {
-                    int c = ((b /10) - 2);
-                    Console.SetCursorPosition((c+25), 3);
-                    Console.WriteLine(b);
+                    int x = b - ((b / 10) * 10);
+                    int c = (x * 2);
+                    Console.SetCursorPosition(c + 27, 3);
+                    Console.WriteLine(" " + b);
                 }
-           
+            }
+
+            if (j > 6 || j <11)
+            {
+
+
+
+                if (b == 10 || b == 20 || b == 30 || b == 40 || b == 50 || b == 60 || b == 70 || b == 80 || b == 90)
+                {
+
+                    Console.SetCursorPosition(48, 5);
+                    Console.WriteLine(" " + b);
+                }
+                else
+                {
+                    int x = b - ((b / 10) * 10);
+                    int c = (x * 2);
+                    Console.SetCursorPosition(c + 27, 5);
+                    Console.WriteLine(" " + b);
+                }
+            }
+
+            if (j >11 || j<16)
+            {
+
+
+
+                if (b == 10 || b == 20 || b == 30 || b == 40 || b == 50 || b == 60 || b == 70 || b == 80 || b == 90)
+                {
+
+                    Console.SetCursorPosition(48, 7);
+                    Console.WriteLine(" " + b);
+                }
+                else
+                {
+                    int x = b - ((b / 10) * 10);
+                    int c = (x * 2);
+                    Console.SetCursorPosition(c + 27, 7);
+                    Console.WriteLine(" " + b);
+                }
+            }
+
+
+        }
+
+        //giocatore2
+
+        for (int i = 0; i < 20; i++)
+        {
+            Console.SetCursorPosition(65, 0);
+            Console.WriteLine("Giocatore 2");
+
+            for (int j = 63; j < 83; j++)
+            {
+                Console.WriteLine("-");
+                Console.SetCursorPosition(j, 4);
+            }
+            for (int j = 63; j < 83; j++)
+            {
+                Console.WriteLine("-");
+                Console.SetCursorPosition(j, 6);
+            }
         }
 
         //tabellone
-        for (int t = 90; t < 300; t++)
+        for (int t = 1; t < 90; t++)
         {
             Thread.Sleep(t);
-            int a = random.Next(1, 90);
+            int a = random.Next(num.Count);
+            int numero = num[a];
+            num.Remove(a);
 
             if (a==10 || a==20 || a == 30 || a == 40 || a == 50 || a == 60 || a == 70 || a == 80 || a == 90)
             {
-                Console.SetCursorPosition(18, ((a / 10)-1));
+                Console.SetCursorPosition(20, ((a / 10)-1));
                 Console.WriteLine(a);
             }
             else 
             {
                 int b = a - ((a/10)*10);
-                int c = ((b * 2)-2);
+                int c = ((b * 2));
                 int d = a / 10;
                 Console.SetCursorPosition(c,d);
                 Console.WriteLine(a);
